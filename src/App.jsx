4d37b6ed6b5aebc344c9d5aac6391849121a6f09
SekoVper <damehlivingstone1@gmail.com>
2023-01-09 11:47:58 +0000

    Setup dev

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <div className="App">
