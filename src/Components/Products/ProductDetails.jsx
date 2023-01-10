6e790ee765cb28841994ce9f8a0bc6b64a8c4e54
SekoVper <damehlivingstone1@gmail.com>
2023-01-10 13:42:46 +0000

    Add product details.

--- src/Components/Products/ProductDetails.jsx
+++ src/Components/Products/ProductDetails.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ProductDetails = () => {
-  console.log('Product detais');
+  const location = useLocation();
+  const product = location.state;
+  console.log('Product quantity', product.length);
   return (
-    <div>
-      <div>ProductDetails</div>
-      <h1>Product Details</h1>
+    <div className="lg:flex p-5">
+      <div>
+        <img src={product.image} alt={product.title} srcSet="" className="" />
+      </div>
+      <div>
+        <h2 className="font-extrabold text-xl my-4">{product.title}</h2>
+        <p className="">{product.category}</p>
+        <p className="font-extrabold my-3 text-xl">
+          $
+          {' '}
+          {product.price}
+          .00
+        </p>
+        <p>{product.description}</p>
+        <button type="button" className="rounded-none bg-green-700 px-2 py-1 my-3 uppercase font-bold text-white">Add To Cart</button>
+      </div>
     </div>
   );
 };
