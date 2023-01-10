798e4fa9cf8f56e5ad3dd1a1b8181614a596042c
SekoVper <damehlivingstone1@gmail.com>
2023-01-10 13:43:46 +0000

    Add styling.

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,28 +1,39 @@
+import { NavLink } from 'react-router-dom';
+
 const Products = ({ products }) => {
   const {
     id, image, title, price,
   } = products;
-  console.log('Products from props', products);
+  console.log('Products from props', products.length);
   return (
-    <div className="flex flex-wrap">
-      {products && products.map((product) => (
-        <div key={product.id} className="basis-1/4 flex justify-center flex-col text-center items-center p-8 cursor-pointer hover:border border-gray-700">
-          <img
-            className="object-contain w-[200px] aspect-square object-contain"
-            src={product.image}
-            alt="picture1"
-          />
-          <div className="mt-4 flex flex-col items-center">
-            <p className="bg-slate-400 w-fit px-3 py-0.5 rounded-full">{product.category}</p>
-            <p>{product.title}</p>
-            <p className="font-bold">
-              $
-              {' '}
-              {product.price}
-            </p>
+    <div>
+      <h3 className='m-4'>
+        Total Stocks:
+        {' '}
+        {products.length}
+      </h3>
+      <div className="grid grid-cols-2">
+        {products && products.map((product) => (
+          <div key={product.id} className="basis-1/4 flex justify-center flex-col text-center items-center p-2 cursor-pointer hover:border border-gray-700">
+            <NavLink to={`/product/${product.id}`} state={product}>
+              <img
+                className="object-contain w-[200px] aspect-square object-contain"
+                src={product.image}
+                alt="picture1"
+              />
+              <div className="mt-4 flex flex-col items-center">
+                <p className="font-bold">{product.category}</p>
+                <p>{product.title}</p>
+                <p className="font-extrabold">
+                  $
+                  {' '}
+                  {product.price}
+                </p>
+              </div>
+            </NavLink>
           </div>
-        </div>
-      )) }
+        )) }
+      </div>
     </div>
   );
 };
