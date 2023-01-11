a4ee8e2ac61a2aa530ed0094b805f7563ccc5b96
SekoVper <damehlivingstone1@gmail.com>
2023-01-11 11:38:19 +0000

    Modify product page

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,11 +4,9 @@ import { searchProduct } from '../../Redux/ProductSlice';
 
 const Products = ({ products }) => {
   const dispatch = useDispatch();
-
-  console.log('Products from props', products.length);
   return (
     <div>
-      <form className="w-4/5">
+      <form className="w-full">
         <label
           htmlFor="default-search"
           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
@@ -16,9 +14,6 @@ const Products = ({ products }) => {
           Search
         </label>
         <div className="relative">
-          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </div>
           <input
             type="search"
             id="default-search"
@@ -27,7 +22,9 @@ const Products = ({ products }) => {
             required
             onChange={(e) => { dispatch(searchProduct(e.target.value)); }}
           />
-          <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </div>
         </div>
       </form>
 
