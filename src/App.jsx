ade20beedf4e832d1a130107eb57da078239ba8a
SekoVper <damehlivingstone1@gmail.com>
2023-01-11 11:34:42 +0000

    Add routing for the product categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Components/Header';
+import Electronics from './Components/Products/Categories/Electronics';
+import Jewelery from './Components/Products/Categories/Jewelery';
+import Men from './Components/Products/Categories/Men';
+import Women from './Components/Products/Categories/Women';
 import ProductDetails from './Components/Products/ProductDetails';
 import ProductsPage from './Components/Products/ProductsPage';
 
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route element={<ProductsPage />} path="/" />
         <Route element={<ProductDetails />} path="/product/:productId" />
+        <Route element={<Jewelery />} path="/Jewelery" />
+        <Route element={<Men />} path="/men" />
+        <Route element={<Women />} path="/women" />
+        <Route element={<Electronics />} path="/electronics" />
       </Routes>
     </Router>
   );
