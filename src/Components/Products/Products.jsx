d4cfd3aed08503aadc74561d9886c64bb724f165
SekoVper <damehlivingstone1@gmail.com>
2023-01-11 10:00:03 +0000

    Implement searching.

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,13 +1,37 @@
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { searchProduct } from '../../Redux/ProductSlice';
 
 const Products = ({ products }) => {
-  const {
-    id, image, title, price,
-  } = products;
+  const dispatch = useDispatch();
+
   console.log('Products from props', products.length);
   return (
     <div>
-      <h3 className='m-4'>
+      <form className="w-4/5">
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-full border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+            placeholder="Search..."
+            required
+            onChange={(e) => { dispatch(searchProduct(e.target.value)); }}
+          />
+          <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+      </form>
+
+      <h3 className="m-4">
         Total Stocks:
         {' '}
         {products.length}
