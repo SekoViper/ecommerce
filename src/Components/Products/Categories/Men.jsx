6e9ddfa917bcf46dc854bf62c2bff2c761a5268f
SekoVper <damehlivingstone1@gmail.com>
2023-01-11 11:41:14 +0000

    Populate men product page

--- src/Components/Products/Categories/Men.jsx
+++ src/Components/Products/Categories/Men.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const Men = () => (
-  <div>Men</div>
-);
+const Men = () => {
+  const products = useSelector((state) => state.products);
+  const menClothing = products.products.filter((product) => product.category === 'men\'s clothing');
+  return (
+    <>
+      <h1 className="m-4 text-3xl">
+        Men&apos;s clothing
+        {' '}
+        {menClothing.length}
+      </h1>
+      <div className="grid grid-cols-2">
+        {menClothing.map((product) => (
+          <div key={product.id} className="basis-1/4 flex justify-center flex-col text-center items-center p-2 cursor-pointer hover:border border-gray-700">
+            <NavLink to={`/product/${product.id}`} state={product}>
+              <img
+                className="object-contain w-[200px] aspect-square object-contain"
+                src={product.image}
+                alt="picture1"
+              />
+              <div className="mt-4 flex flex-col items-center">
+                <p className="font-bold">{product.category}</p>
+                <p>{product.title}</p>
+                <p className="font-extrabold">
+                  $
+                  {' '}
+                  {product.price}
+                </p>
+              </div>
+            </NavLink>
+          </div>
+        ))}
+      </div>
+
+    </>
+  );
+};
 
 export default Men;
