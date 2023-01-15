40be1bcfe10969f2d01536f1bdd5f5bdaf3aef7d
SekoVper <damehlivingstone1@gmail.com>
2023-01-15 23:47:12 +0000

    Add responsiveness

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 const Products = ({ products }) => (
   <div className="mx-5">
-    <div className="grid grid-cols-2 lg:bg-[red] ">
+    <div className="grid grid-cols-2">
       {products && products.map((product) => (
         <div key={product.id} className="basis-1/4 bg-white m-1 rounded-lg flex justify-center flex-col text-center items-center p-2 cursor-pointer hover:border border-gray-700">
           <NavLink to={`/product/${product.id}`} state={product}>
             <img
-              className="object-contain w-[200px] aspect-square object-contain"
+              className="object-contain aspect-square object-contain"
               src={product.image}
               alt="picture1"
             />
-            <div className="mt-4 flex flex-col items-center">
+            <div className="mt-4 flex flex-col text-center items-center">
               <p className="font-bold">{product.category}</p>
               <p>{product.title}</p>
               <p className="font-extrabold">
