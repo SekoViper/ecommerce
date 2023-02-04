c78b21ec53239430fa91f547ca6d3bdc1fe1ba73
SekoVper <damehlivingstone1@gmail.com>
2023-02-04 13:48:00 +0000

    Refactor

--- src/Components/Products/ProductDetails.jsx
+++ src/Components/Products/ProductDetails.jsx
@@ -9,6 +9,10 @@ const ProductDetails = () => {
   const product = location.state;
   const dispatch = useDispatch();
 
+  const {
+    title, description, price, image, category,
+  } = product;
+
   return (
     <div className="mx-5">
       <div>
@@ -18,24 +22,22 @@ const ProductDetails = () => {
           {' '}
         </NavLink>
       </div>
-      <div className="lg:flex p-5 bg-white rounded-lg mt-5">
-        <div>
-          <img src={product.image} alt={product.title} />
+      <div className="lg:flex lg:items-center lg:mx-12 lg:gap-5 p-5 bg-white rounded-lg mt-5">
+        <div className="text-center">
+          <img className="lg:w-1/2" src={image} alt={title} />
         </div>
         <div>
-          <h2 className="font-extrabold text-xl my-4">{product.title}</h2>
-          <p className="">{product.category}</p>
+          <h2 className="font-extrabold text-xl my-4">{title}</h2>
+          <p className="">{category}</p>
           <p className="font-extrabold my-3 text-xl">
             $
             {' '}
-            {product.price}
+            {price}
             .00
           </p>
-          <p>{product.description}</p>
+          <p>{description}</p>
           <button
-            onClick={() => dispatch(addToCart(
-              product.id,
-            ))}
+            onClick={() => dispatch(addToCart(product))}
             type="button"
             className="rounded-none bg-green-700 px-2 py-1 my-3 uppercase font-bold text-white"
           >
