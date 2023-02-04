29bde636b3ad5e69ef51a4c4491d2a3a4ff64345
SekoVper <damehlivingstone1@gmail.com>
2023-02-04 13:48:48 +0000

    Refactor

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,46 +1,32 @@
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../Redux/ProductSlice';
 
-const Products = ({ products }) => {
-  const dispatch = useDispatch();
-  return (
-    <div className="mx-5">
-      <div className="grid grid-cols-2 col-desktop">
-        {products && products.map((product) => (
-          <div key={product.id} className="basis-1/4 bg-white m-1 rounded-lg flex justify-center flex-col text-center items-center p-2 cursor-pointer hover:border border-gray-700">
-            <NavLink to={`/product/${product.id}`} state={product}>
-              <img
-                className="object-contain aspect-square object-contain"
-                src={product.image}
-                alt="picture1"
-              />
-              <div className="mt-4 flex flex-col text-center items-center">
-                <p className="font-bold">{product.category}</p>
-                <p>{product.title}</p>
-                <p className="font-extrabold">
-                  $
-                  {' '}
-                  {product.price}
-                </p>
-              </div>
-            </NavLink>
-            <button
-              onClick={() => dispatch(addToCart(
-                id,
-              ))}
-              type="button"
-              className="rounded-none bg-green-700 px-2 py-1 my-3 uppercase font-bold text-white"
-            >
-              Add To Cart
-            </button>
-          </div>
-        )) }
-      </div>
+const Products = ({ products }) => (
+  <div className="mx-5">
+    <div className="grid grid-cols-2 col-desktop">
+      {products && products.map((product) => (
+        <div key={product.id} className="basis-1/4 bg-white m-1 rounded-lg flex justify-center flex-col text-center items-center p-2 cursor-pointer hover:border border-gray-700">
+          <NavLink to={`/product/${product.id}`} state={product}>
+            <img
+              className="object-contain aspect-square object-contain"
+              src={product.image}
+              alt="picture1"
+            />
+            <div className="mt-4 flex flex-col text-center items-center">
+              <p className="font-bold">{product.category}</p>
+              <p>{product.title}</p>
+              <p className="font-extrabold">
+                $
+                {' '}
+                {product.price}
+              </p>
+            </div>
+          </NavLink>
+        </div>
+      )) }
     </div>
-  );
-};
+  </div>
+);
 
 Products.defaultProps = { products: null };
 Products.propTypes = {
